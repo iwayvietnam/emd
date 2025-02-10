6b2bf2f1b6ba52cf18de0e0f243bfdbbd4bbff10
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-02-10 13:20:31 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Filament/Resources/SenderTransportResource/Pages/ListSenderTransports.php
+++ admin/app/Filament/Resources/SenderTransportResource/Pages/ListSenderTransports.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\SenderTransportResource\Pages;
 
 use App\Filament\Resources\SenderTransportResource;
-use Filament\Actions;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 /**
@@ -20,7 +20,7 @@ class ListSenderTransports extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make()->label(__("New Sender Transport")),
         ];
     }
 }
