d9a76f27948de392229fffc22d58b94a7efc6d4b
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-03-05 09:30:10 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Filament/Pages/EditUserProfile.php
+++ admin/app/Filament/Pages/EditUserProfile.php
@@ -5,8 +5,17 @@ namespace App\Filament\Pages;
 use Filament\Forms\Components\Component;
 use Filament\Pages\Auth\EditProfile;
 
+/**
+ * Edit user profile class
+ *
+ * @package  App
+ * @category Filament
+ * @author   Nguyen Van Nguyen - nguyennv@iwayvietnam.com
+ */
 class EditUserProfile extends EditProfile
 {
+    protected static ?string $navigationGroup = 'System';
+
     protected function getEmailFormComponent(): Component
     {
         return parent::getEmailFormComponent()->readonly();
