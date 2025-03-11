a6b73ed5b834e6b02ddab23caf64925b4a3578f4
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-03-11 13:21:15 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/resources/views/filament/pages/send-email.blade.php
+++ admin/resources/views/filament/pages/send-email.blade.php
@@ -1,5 +1,5 @@
 <x-filament-panels::page>
-    <x-filament-panels::form>
+    <x-filament-panels::form wire:submit="send">
         {{ $this->form }}
         <x-filament-panels::form.actions
             :actions="$this->getFormActions()"
