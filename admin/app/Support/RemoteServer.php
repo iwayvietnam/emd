57677fc4c54416ca8db27c188cc8009aa2e4a4e1
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-03-13 10:56:24 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Support/RemoteServer.php
+++ admin/app/Support/RemoteServer.php
@@ -67,17 +67,7 @@ class RemoteServer
             }
 
             if (!empty($this->ssh->getStdError())) {
-                // logger()->error($this->ssh->getStdError());
-                throw new \RuntimeException(
-                    strtr(
-                        "Error running command {command} on server {remoteHost}: {message}",
-                        [
-                            "{command}" => $command,
-                            "{remoteHost}" => $this->remoteHost,
-                            "{message}" => $this->ssh->getStdError(),
-                        ]
-                    )
-                );
+                logger()->error($this->ssh->getStdError());
             }
         } catch (\Throwable $th) {
             throw new \RuntimeException(
