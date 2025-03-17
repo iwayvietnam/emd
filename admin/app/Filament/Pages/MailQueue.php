509fe7c8441f51ce209aeef4ac7213fd7e11be68
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-03-17 16:25:12 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Filament/Pages/MailQueue.php
+++ admin/app/Filament/Pages/MailQueue.php
@@ -2,6 +2,11 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\MailServer;
+use Filament\Actions\Action;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
 
 /**
@@ -11,15 +16,43 @@ use Filament\Pages\Page;
  * @category Filament
  * @author   Nguyen Van Nguyen - nguyennv@iwayvietnam.com
  */
-class MailQueue extends Page
+class MailQueue extends Page implements HasForms
 {
+    use InteractsWithForms;
+
     protected static ?string $navigationGroup = "System";
     protected static ?string $navigationIcon = "heroicon-o-envelope";
     protected static ?string $slug = 'mail queue';
     protected static string $view = "filament.pages.mail-queue";
 
+    /**
+     * @var array<string, mixed> | null
+     */
+    public ?array $data = [];
+
     public static function getNavigationLabel(): string
     {
         return __('Mail Queue');
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make("mail_server")
+                    ->options(MailServer::all()->pluck("name", "id"))
+                    ->required()
+                    ->label(__("Mail Server")),
+            ])
+            ->statePath("data");
+    }
+
+    protected function getFormActions(): array
+    {
+        return [Action::make("list")->label(__("List Mail Queue"))->submit("listMailQueue")];
+    }
+
+    public function listMailQueue(): void
+    {
     }
 }
