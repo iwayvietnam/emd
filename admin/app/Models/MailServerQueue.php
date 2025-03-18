4370a2c05f2d2837ce7b3119f634c8545ec96331
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-03-18 17:12:55 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Models/MailServerQueue.php
+++ admin/app/Models/MailServerQueue.php
@@ -21,14 +21,22 @@ class MailServerQueue extends Model
         $formState = session()->get(MailServerQueue::class);
         $server = MailServer::find($formState['mail_server'] ?? 0);
         $queues = collect($server?->listQueue() ?? []);
+
         $sender = $formState['sender'] ?? '';
-        $recipient = $formState['recipient'] ?? '';
-        if (!empty($sender) || !empty($recipient)) {
-            $queues = $queues->filter(function ($queue) use ($sender, $recipient) {
-                return str($queue['sender'])->contains([$sender, $recipient]);
+        if (!empty($sender)) {
+            $queues = $queues->filter(function ($queue) use ($sender) {
+                return str($queue['sender'])->contains($sender);
             });
         }
-        return $queues->toArray();
+
+        $recipient = $formState['recipient'] ?? '';
+        if (!empty($recipient)) {
+            $queues = $queues->filter(function ($queue) use ($sender, $recipient) {
+                return str($queue['recipients'])->contains($recipient);
+            });
+        }
+
+        return array_values($queues->toArray());
     }
 
     protected function sushiShouldCache()
