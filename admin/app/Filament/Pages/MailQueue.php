ade5fa7c2f2b0ac8a6d6b54c0181ffade9978318
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-03-24 13:27:11 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Filament/Pages/MailQueue.php
+++ admin/app/Filament/Pages/MailQueue.php
@@ -4,6 +4,7 @@ namespace App\Filament\Pages;
 
 use App\Models\MailServer;
 use App\Models\MailServerQueue;
+use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Forms\Components\Actions\Action as FormAction;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Select;
@@ -29,6 +30,7 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class MailQueue extends Page implements HasTable
 {
+    use InteractsWithActions;
     use InteractsWithTable;
 
     protected static ?string $navigationGroup = "System";
