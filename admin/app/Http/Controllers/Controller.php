54ed91574629227f330887f484950a3d2b726cab
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-03-27 15:12:48 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Http/Controllers/Controller.php
+++ admin/app/Http/Controllers/Controller.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * Abstract controller.
+ *
+ * @package  App
+ * @category Http
+ * @author   Nguyen Van Nguyen - nguyennv@iwayvietnam.com
+ */
 abstract class Controller
 {
-    //
+    /**
+     * Constructor
+     * Create a new controller instance.
+     *
+     * @return self
+     */
+    public function __construct() {}
 }
