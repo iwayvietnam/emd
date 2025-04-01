aa6e2097be71f37ce2e6092619631bd058c51392
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-04-01 16:00:44 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/routes/api.php
+++ admin/routes/api.php
@@ -10,17 +10,14 @@ Route::get(
     static fn (Request $request) => $request->user()
 )->middleware("auth:api");
 
-Route::controller(EmailController::class)
-    ->group(static function () {
-        Route::get("/email", "index");
-        Route::get("/email/{id}", "show");
-        Route::get("/email/{id}/devices", "devices");
-    })->middleware("auth:api");
-
-Route::post("/send", [
-    EmailController::class,
-    "send",
-])->middleware("auth:api");
+Route::middleware("auth:api")->controller(
+    EmailController::class
+)->group(static function () {
+    Route::get("/email", "index");
+    Route::get("/email/{id}", "show");
+    Route::get("/email/{id}/devices", "devices");
+    Route::post("/send", "send");
+});
 
 Route::post("/upload", [
     UploadController::class,
