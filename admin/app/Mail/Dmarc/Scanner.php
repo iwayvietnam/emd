8d14b36e9ed6832c015cf6c7ca86e40c80435a61
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-04-04 08:58:36 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Mail/Dmarc/Scanner.php
+++ admin/app/Mail/Dmarc/Scanner.php
@@ -60,84 +60,12 @@ final class Scanner
                 foreach ($reports as $report) {
                     $dmarcReports[] = $report;
                     if (!empty($report->report_metadata)) {
-                        DmarcReport::where([
+                        $models[] = DmarcReport::where([
                             ["report_id", $report->report_metadata->report_id],
                             ["org_name", $report->report_metadata->org_name],
-                        ])->firstOr(function () use ($report, $models) {
-                            $metadata = $report->report_metadata;
-                            $policy = $report->policy_published;
-                            $models[] = DmarcReport::create([
-                                "report_id" => $metadata?->report_id ?? "",
-                                "org_name" => $metadata?->org_name ?? "",
-                                "org_email" => $metadata?->email ?? null,
-                                "extra_contact" =>
-                                    $metadata?->xtra_contact_info ?? null,
-                                "date_begin" => Carbon::createFromTimestampUTC(
-                                    (int) $metadata?->date_range?->begin
-                                ),
-                                "date_end" => Carbon::createFromTimestampUTC(
-                                    (int) $metadata?->date_range?->end
-                                ),
-                                "domain" => $policy?->domain ?? "",
-                                "adkim" => $policy?->adkim ?? "",
-                                "aspf" => $policy?->aspf ?? "",
-                                "policy" => $policy?->p ?? "",
-                                "subdomain_policy" => $policy?->sp ?? "",
-                                "percentage" => (int) ($policy?->pct ?? 0),
-                                "is_forensic" => (bool) ($policy?->fo ?? false),
-                                "created_by" => auth()->user()->id,
-                            ]);
-
-                            if (!empty($report->records)) {
-                                foreach ($report->records as $record) {
-                                    DmarcReportRecord::create([
-                                        "report_id" =>
-                                            $metadata?->report_id ?? "",
-                                        "source_ip" =>
-                                            $record?->row?->source_ip ?? "",
-                                        "count" =>
-                                            (int) ($record?->row?->count ?? 0),
-                                        "header_from" =>
-                                            $record?->identifiers
-                                                ?->header_from ?? "",
-                                        "envelope_from" =>
-                                            $record?->identifiers
-                                                ?->envelope_from ?? null,
-                                        "envelope_to" =>
-                                            $record?->identifiers
-                                                ?->envelope_to ?? null,
-                                        "disposition" =>
-                                            $record?->row?->policy_evaluated
-                                                ?->disposition ?? "",
-                                        "dkim" =>
-                                            $record?->row?->policy_evaluated
-                                                ?->dkim ?? "",
-                                        "spf" =>
-                                            $record?->row?->policy_evaluated
-                                                ?->spf ?? "",
-                                        "reason" =>
-                                            $record?->row?->policy_evaluated
-                                                ?->reason?->comment ?? null,
-                                        "dkim_domain" =>
-                                            $record?->auth_results?->dkim
-                                                ?->domain ?? null,
-                                        "dkim_selector" =>
-                                            $record?->auth_results?->dkim
-                                                ?->selector ?? null,
-                                        "dkim_result" =>
-                                            $record?->auth_results?->dkim
-                                                ?->result ?? null,
-                                        "spf_domain" =>
-                                            $record?->auth_results?->spf
-                                                ?->domain ?? null,
-                                        "spf_result" =>
-                                            $record?->auth_results?->spf
-                                                ?->result ?? null,
-                                        "created_by" => auth()->user()->id,
-                                    ]);
-                                }
-                            }
-                        });
+                        ])->firstOr(
+                            static fn() => self::createDmarcReport($report)
+                        );
 
                         if ($canMove) {
                             $message->move($archiveFolder);
@@ -153,13 +81,74 @@ final class Scanner
                 }
             }
 
-            $this->indexReports($dmarcReports);
+            $this->indexDmarcReports($dmarcReports);
         }
 
         return $models;
     }
 
-    private function indexReports(array $reports): void
+    private static function createDmarcReport($report): array
+    {
+        $reports = [];
+
+        $metadata = $report->report_metadata;
+        $policy = $report->policy_published;
+        $reports[] = DmarcReport::create([
+            "report_id" => $metadata?->report_id ?? "",
+            "org_name" => $metadata?->org_name ?? "",
+            "org_email" => $metadata?->email ?? null,
+            "extra_contact" => $metadata?->xtra_contact_info ?? null,
+            "date_begin" => Carbon::createFromTimestampUTC(
+                (int) $metadata?->date_range?->begin
+            ),
+            "date_end" => Carbon::createFromTimestampUTC(
+                (int) $metadata?->date_range?->end
+            ),
+            "domain" => $policy?->domain ?? "",
+            "adkim" => $policy?->adkim ?? "",
+            "aspf" => $policy?->aspf ?? "",
+            "policy" => $policy?->p ?? "",
+            "subdomain_policy" => $policy?->sp ?? "",
+            "percentage" => (int) ($policy?->pct ?? 0),
+            "is_forensic" => (bool) ($policy?->fo ?? false),
+        ]);
+
+        if (!empty($report->records)) {
+            foreach ($report->records as $record) {
+                DmarcReportRecord::create([
+                    "report_id" => $metadata?->report_id ?? "",
+                    "source_ip" => $record?->row?->source_ip ?? "",
+                    "count" => (int) ($record?->row?->count ?? 0),
+                    "header_from" => $record?->identifiers?->header_from ?? "",
+                    "envelope_from" =>
+                        $record?->identifiers?->envelope_from ?? null,
+                    "envelope_to" =>
+                        $record?->identifiers?->envelope_to ?? null,
+                    "disposition" =>
+                        $record?->row?->policy_evaluated?->disposition ?? "",
+                    "dkim" => $record?->row?->policy_evaluated?->dkim ?? "",
+                    "spf" => $record?->row?->policy_evaluated?->spf ?? "",
+                    "reason" =>
+                        $record?->row?->policy_evaluated?->reason?->comment ??
+                        null,
+                    "dkim_domain" =>
+                        $record?->auth_results?->dkim?->domain ?? null,
+                    "dkim_selector" =>
+                        $record?->auth_results?->dkim?->selector ?? null,
+                    "dkim_result" =>
+                        $record?->auth_results?->dkim?->result ?? null,
+                    "spf_domain" =>
+                        $record?->auth_results?->spf?->domain ?? null,
+                    "spf_result" =>
+                        $record?->auth_results?->spf?->result ?? null,
+                ]);
+            }
+        }
+
+        return $reports;
+    }
+
+    private function indexDmarcReports(array $reports): void
     {
         if ($this->elasticClient instanceof ElasticClient && !empty($reports)) {
             $params = [
