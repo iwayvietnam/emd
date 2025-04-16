ceb145f7301ed88aea918e3729e9c54e366efcca
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-04-16 15:58:07 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Providers/Filament/AdminPanelProvider.php
+++ admin/app/Providers/Filament/AdminPanelProvider.php
@@ -37,7 +37,7 @@ class AdminPanelProvider extends PanelProvider
             ->id(config("emd.panel.id", self::NAME))
             ->path(config("emd.panel.path", self::NAME))
             ->topNavigation(config("emd.panel.top_navigation", false))
-            ->domain(config("emd.app_domain"))
+            // ->domain(config("emd.app_domain"))
             ->login()
             ->profile(EditUserProfile::class, isSimple: false)
             ->colors([
