6e455f7e28b2651e29e5f0f46a88a64343c8ea75
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-10-20 17:09:04 +0700

    Update ListDkimKeys.php

--- admin/app/Filament/Resources/DkimKeyResource/Pages/ListDkimKeys.php
+++ admin/app/Filament/Resources/DkimKeyResource/Pages/ListDkimKeys.php
@@ -4,6 +4,11 @@ namespace App\Filament\Resources\DkimKeyResource\Pages;
 
 use App\Filament\Resources\DkimKeyResource;
 use Filament\Actions\CreateAction;
+use App\Models\DkimKey;
+use App\Models\MailServer;
+use Filament\Actions;
+use Filament\Forms\Components\Select;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 /**
@@ -26,6 +31,44 @@ class ListDkimKeys extends ListRecords
     {
         return [
             CreateAction::make()->label(__("New DKIM Key")),
+            Actions\Action::make("syncKeys")
+                ->form([
+                    Select::make("mail_server")
+                        ->options(MailServer::all()->pluck("name", "id"))
+                        ->required()
+                        ->label(__("Mail Server")),
+                ])
+                ->action(
+                    static fn(array $data) => self::syncOpenDkimKeys(
+                        (int) $data["mail_server"]
+                    )
+                )
+                ->label(__("Sync To Mail Server")),
         ];
+    }
+
+    private static function syncOpenDkimKeys(int $id): void
+    {
+        $signingTable = DkimKey::signingTable();
+        $keyTable = DkimKey::keyTable();
+        $privateKeys = DkimKey::privateKeys();
+
+        if (!empty($signingTable) || !empty($keyTable) || !empty($privateKeys)) {
+            try {
+                MailServer::find($id)->syncOpenDkimKeys($signingTable, $keyTable, $privateKeys);
+            } catch (\Throwable $th) {
+                logger()->error($th);
+                Notification::make()
+                    ->title(__("Failed to synchronize OpenDKIM keys!"))
+                    ->danger()
+                    ->send();
+                return;
+            }
+        }
+
+        Notification::make()
+            ->title(__("OpenDKIM keys have been synchronized!"))
+            ->success()
+            ->send();
     }
 }
