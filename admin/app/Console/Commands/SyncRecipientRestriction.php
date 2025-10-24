32ffe0f4e98b21ea7d919f78659ec15e539b91d9
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-10-24 16:12:38 +0700

    WIP

--- admin/app/Console/Commands/SyncRecipientRestriction.php
+++ admin/app/Console/Commands/SyncRecipientRestriction.php
@@ -3,36 +3,36 @@
 namespace App\Console\Commands;
 
 use App\Models\MailServer;
-use App\Models\SenderTransport;
+use App\Models\RestrictedRecipient;
 use App\Support\RemoteServer;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\Isolatable;
 use Symfony\Component\Console\Attribute\AsCommand;
 
 /**
- * Sync sender transport command
- * Synchronize sender transports to mail server.
+ * Sync recipient restriction command
+ * Synchronize restricted recipients to mail server.
  *
  * @package  App
  * @category Console
  * @author   Nguyen Van Nguyen - nguyennv@iwayvietnam.com
  */
-#[AsCommand(name: "sync:tranport")]
-class SyncSenderTransport extends Command implements Isolatable
+#[AsCommand(name: "sync:restriction")]
+class SyncRecipientRestriction extends Command implements Isolatable
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = "sync:tranport {server}";
+    protected $signature = "sync:restriction {server}";
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = "Synchronize sender transports to mail server.";
+    protected $description = "Synchronize restricted recipients to mail server.";
 
     /**
      * Execute the console command.
@@ -41,12 +41,12 @@ class SyncSenderTransport extends Command implements Isolatable
      */
     public function handle(): int
     {
-        $this->syncTransport($this->argument("server"));
-        $this->info("Sync sender transport command was successful!");
+        $this->syncRestriction($this->argument("server"));
+        $this->info("Sync recipient restriction command was successful!");
         return Command::SUCCESS;
     }
 
-    private function syncTransport(string $server): void
+    private function syncRestriction(string $server): void
     {
         if (filter_var($server, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
             $server = MailServer::firstWhere("ip_address", $server);
@@ -54,9 +54,9 @@ class SyncSenderTransport extends Command implements Isolatable
             $server = MailServer::firstWhere("name", $server);
         }
         if ($server->id) {
-            $transports = SenderTransport::transports();
-            if (!empty($transports)) {
-                $server->syncSenderTransports($transports);
+            $restrictions = RestrictedRecipient::recipientRestrictions();
+            if (!empty($restrictions)) {
+                $server->syncRecipientRestrictions($restrictions);
             }
         }
     }
