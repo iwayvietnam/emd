4175c7e49830b7537b1951fb71598b89ca2385cf
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-10-27 15:10:58 +0700

    Update MailServer.php

--- admin/app/Models/MailServer.php
+++ admin/app/Models/MailServer.php
@@ -260,7 +260,7 @@ class MailServer extends Model
         string $file,
     ): void {
         if (!empty($table)) {
-            $tempFile = basename(tempnam(sys_get_temp_dir(), "postfix"));
+            $tempFile = basename($file);
             $remoteServer = new RemoteServer(
                 $this->ip_address,
                 $this->ssh_port,
