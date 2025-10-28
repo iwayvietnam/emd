40a4e7c78437f6dbcbf9891866c3c5fea071defb
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2025-10-28 10:47:19 +0700

    Update app.php

--- admin/bootstrap/app.php
+++ admin/bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(
+            at: [
+                '192.168.1.1',
+            ],
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
