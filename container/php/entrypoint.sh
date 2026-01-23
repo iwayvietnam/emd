3f5cf46813baf98230d848145f84a32bace685da
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2026-01-23 10:22:30 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- container/php/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-export PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin
-
-php /var/www/html/artisan serve --host=0.0.0.0 --port=80 >/dev/null 2>&1
-php /var/www/html/artisan queue:work >/dev/null 2>&1
-php /var/www/html/artisan policy:listen start >/dev/null 2>&1
