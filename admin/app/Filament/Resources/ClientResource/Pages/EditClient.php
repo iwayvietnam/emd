b9372dac309c9e26b8b68a3699ac12e1b745ade1
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2026-02-12 15:13:28 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Filament/Resources/ClientResource/Pages/EditClient.php
+++ admin/app/Filament/Resources/ClientResource/Pages/EditClient.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\ClientResource\Pages;
 use App\Filament\Resources\ClientResource;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Schema;
 use Filament\Resources\Pages\EditRecord;
 
@@ -22,14 +23,17 @@ class EditClient extends EditRecord
     public function form(Schema $schema): Schema
     {
         return $schema->components([
-            TextInput::make("name")
-                ->readonly()
+            Grid::make(3)
                 ->columnSpan(2)
-                ->label(__("Name")),
-            TextInput::make("sender_address")
-                ->readonly()
-                ->label(__("Sender Address")),
-            TextInput::make("bcc_address")->email()->label(__("Bcc Address")),
+                ->schema([
+                    TextInput::make("name")->columnSpan(2)->label(__("Name")),
+                    TextInput::make("sender_address")
+                        ->readonly()
+                        ->label(__("Sender Address")),
+                    TextInput::make("bcc_address")
+                        ->email()
+                        ->label(__("Bcc Address")),
+                ]),
             Textarea::make("description")
                 ->columnSpan(2)
                 ->label(__("Description")),
