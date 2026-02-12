cb3030f8fbaecf6e888b22f63f23db9566cf9752
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2026-02-12 15:33:06 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Filament/Pages/SendEmail.php
+++ admin/app/Filament/Pages/SendEmail.php
@@ -135,7 +135,7 @@ class SendEmail extends Page
                 $this->form->fill();
                 if ($shouldQueue) {
                     Mail::to($message->recipient)->queue(
-                        new SendMessage($message)->onQueue(
+                        (new SendMessage($message))->onQueue(
                             config("emd.mail.queue_name", self::QUEUE_NAME),
                         ),
                     );
