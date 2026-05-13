970cc5881ac195509471101d62a76e501ebf76f0
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2026-05-13 15:22:01 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ admin/app/Enums/Avatar.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Enums;
+
+/**
+ * Avatar enum
+ *
+ * @package  App
+ * @category Enums
+ * @author   Nguyen Van Nguyen - nguyennv@iwayvietnam.com
+ */
+enum Avatar: string {
+    case ElectricalWorker = "electrical-worker.svg";
+    case Mechanic = "mechanic.svg";
+    case Specialist = "specialist.svg";
+    case User = "user.svg";
+    case UserAvatar = "user-avatar.svg";
+    case UserAvatarFilled = "user-avatar-filled.svg";
+    case UserAvatarFilledAlt = "user-avatar-filled-alt.svg";
+    case UserAvatarProfile = "user-avatar-profile.svg";
+    case UserSilhouette = "user-silhouette.svg";
+    case UserSocial = "user-social.svg";
+}
