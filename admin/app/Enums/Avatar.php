1940b665feff6620897a7846e5850dff50b5580f
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2026-05-13 15:59:41 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Enums/Avatar.php
+++ admin/app/Enums/Avatar.php
@@ -14,10 +14,12 @@ enum Avatar: string {
     case Mechanic = "mechanic.svg";
     case Specialist = "specialist.svg";
     case User = "user.svg";
+    case UserAlt = "user-alt.svg";
     case UserAvatar = "user-avatar.svg";
     case UserAvatarFilled = "user-avatar-filled.svg";
     case UserAvatarFilledAlt = "user-avatar-filled-alt.svg";
     case UserAvatarProfile = "user-avatar-profile.svg";
     case UserSilhouette = "user-silhouette.svg";
     case UserSocial = "user-social.svg";
+    case UserSocialAlt = "user-social-alt.svg";
 }
