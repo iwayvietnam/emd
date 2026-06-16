3c17f73635065f44c3132fca87f2ea882db73aa2
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2026-06-16 07:13:42 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Workers/PolicyWorker.php
+++ admin/app/Workers/PolicyWorker.php
@@ -2,12 +2,33 @@
 
 namespace App\Workers;
 
+use Spiral\RoadRunner\Worker;
+use Spiral\RoadRunner\Tcp\TcpWorker;
+use Spiral\RoadRunner\Tcp\TcpResponse;
+use Spiral\RoadRunner\Tcp\TcpEvent;
 use Spiral\RoadRunnerLaravel\WorkerInterface;
 use Spiral\RoadRunnerLaravel\WorkerOptionsInterface;
 
 class PolicyWorker implements WorkerInterface
 {
-	public function start(WorkerOptionsInterface $options): void
-	{
-	}
+    public function start(WorkerOptionsInterface $options): void
+    {
+        $worker = Worker::create();
+        $tcpWorker = new TcpWorker($worker);
+        while ($request = $tcpWorker->waitRequest()) {
+            try {
+                if ($request->event === TcpEvent::Connected) {
+                }
+
+                if ($request->event === TcpEvent::Data) {
+                }
+
+                if ($request->event === TcpEvent::Close) {
+                }
+            } catch (\Throwable $e) {
+                $tcpWorker->respond("Something went wrong\r\n", TcpResponse::RespondClose);
+                $worker->error((string)$e);
+            }
+        }
+    }
 }
