f8abe3abc4d4ea7d2fef2005eb53166dc81db6d3
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2026-06-22 08:57:34 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- admin/app/Workers/PolicyWorker.php
+++ admin/app/Workers/PolicyWorker.php
@@ -2,6 +2,9 @@
 
 namespace App\Workers;
 
+use App\Mail\Policy\Policy;
+use App\Mail\Policy\PolicyRequest;
+use Illuminate\Support\Facades\Log;
 use Spiral\RoadRunner\Worker;
 use Spiral\RoadRunner\Tcp\TcpWorker;
 use Spiral\RoadRunner\Tcp\TcpResponse;
@@ -9,25 +12,61 @@ use Spiral\RoadRunner\Tcp\TcpEvent;
 use Spiral\RoadRunnerLaravel\WorkerInterface;
 use Spiral\RoadRunnerLaravel\WorkerOptionsInterface;
 
+/**
+ * Policy worker
+ *
+ * @package  App
+ * @category Workers
+ * @author   Nguyen Van Nguyen - nguyennv@iwayvietnam.com
+ */
 class PolicyWorker implements WorkerInterface
 {
     public function start(WorkerOptionsInterface $options): void
     {
-        $worker = Worker::create();
-        $tcpWorker = new TcpWorker($worker);
+        $tcpWorker = new TcpWorker(Worker::create());
         while ($request = $tcpWorker->waitRequest()) {
             try {
                 if ($request->event === TcpEvent::Connected) {
+                    Log::debug(
+                        "Access policy {remote_ip} connect at {time}.",
+                        [
+                            "remote_ip" => $request->getRemoteAddress(),
+                            "time" => microtime(true),
+                        ],
+                    );
                 }
 
                 if ($request->event === TcpEvent::Data) {
+                    $start = hrtime(true);
+                    var $policy = new Policy();
+                    var $action = $policy->check(
+                        PolicyRequest::fromData($request->getBody())
+                    )->getAction();
+                    Log::debug(
+                        "Policy service checked client access in {elapsed_time} ms.",
+                        [
+                            "elapsed_time" => (hrtime(true) - $start) / 1_000_000,
+                        ],
+                    );
+                    $tcpWorker->respond($action . PHP_EOL . PHP_EOL, TcpResponse::RespondClose);
                 }
 
                 if ($request->event === TcpEvent::Close) {
+                    Log::debug(
+                        "Access policy {remote_ip} closed at {time}.",
+                        [
+                            "remote_ip" => $request->getRemoteAddress(),
+                            "time" => microtime(true),
+                        ],
+                    );
+                }
+
+                if ($request->event === TcpEvent::Unknown) {
+                    $tcpWorker->respond("", TcpResponse::RespondClose);
                 }
             } catch (\Throwable $e) {
                 $tcpWorker->respond("Something went wrong\r\n", TcpResponse::RespondClose);
-                $worker->error((string)$e);
+                Log::error((string)$e);
             }
         }
     }
